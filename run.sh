7bf3980f878804fd459b287dbe3ee44d73a62690
preethu thomas <preethu.thomas@ufl.edu>
2014-11-06 14:24:09 -0500

    Removing all code

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sbt package
-rm -rf op
-export HADOOP_CLASSPATH=/home/$USER/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.10.4.jar:/home/$USER/.ivy2/cache/commons-configuration/commons-configuration/jars/commons-configuration-1.10.jar:/home/$USER/.ivy2/cache/commons-lang/commons-lang/jars/commons-lang-2.6.jar
-LIBJARS=/home/$USER/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.10.4.jar,/home/$USER/.ivy2/cache/commons-configuration/commons-configuration/jars/commons-configuration-1.10.jar,/home/$USER/.ivy2/cache/commons-lang/commons-lang/jars/commons-lang-2.6.jar
-
-/opt/hadoop-1.2.1/bin/hadoop jar /home/$USER/IdeaProjects/e2/target/scala-2.10/eel-fall2014-project_2.10-1.0.jar -libjars $LIBJARS src/main/resources/410119.csv op src/main/resources/parse-config.properties
--- src/main/scala/FuzzyTimeSeries/AnnualRecord.scala
+++
@@ -1,5 +0,0 @@
-package FuzzyTimeSeries
-
-case class AnnualRecord(timeSlot: String, events: Int,
-                        var fuzzySet: String = "", var flrgLH: String = "",
-                        var flrgRH: String = "", var fcEvents:Int = 0)
